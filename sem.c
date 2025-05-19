f03200d032db76615113c37582df429e5d8a6648
sarthak <sarthak.bosamiya@einfochips.com>
2025-05-19 15:30:01 +0530

    Initial Commit

--- sem.c
+++ sem.c
@@ -21,7 +21,7 @@ void unlock() {
 void* func(void* arg){
 	lock();
 	for(int i=0; i<5; i++){
-		printf("Thread id = %d\n", *(int*)arg);
+		printf("%d: Thread id = %d\n", i, *(int*)arg);
 		sleep(2);
 	}
 	unlock();
@@ -30,11 +30,13 @@ void* func(void* arg){
 
 int main() {
     	sem_init(&sem, 0, 1);
-	pthread_t t1, t2;
-	int id1 = 1, id2 = 2;
+	pthread_t t1, t2, t3;
+	int id1 = 1, id2 = 2, id3 = 3;
 	pthread_create(&t1, NULL, func, &id1);
 	pthread_create(&t2, NULL, func, &id2);
+	pthread_create(&t3, NULL, func, &id3);
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+	pthread_join(t3, NULL);
 	return 0;
 }
